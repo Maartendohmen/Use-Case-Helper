52d4a6188ee34d3c22a3a13416a36c19f340c84c
Maartendohmen <maartendohmen@gmail.com>
2017-02-14 13:43:09 +0100

    Added Man v1

--- Use Case Helper/Form1.Designer.cs
+++ Use Case Helper/Form1.Designer.cs
@@ -29,18 +29,20 @@
         private void InitializeComponent()
         {
             this.gblements = new System.Windows.Forms.GroupBox();
-            this.rbtnactor = new System.Windows.Forms.RadioButton();
-            this.rbtnusecases = new System.Windows.Forms.RadioButton();
             this.rbtnline = new System.Windows.Forms.RadioButton();
+            this.rbtnusecases = new System.Windows.Forms.RadioButton();
+            this.rbtnactor = new System.Windows.Forms.RadioButton();
             this.gpbmode = new System.Windows.Forms.GroupBox();
-            this.rbtncreate = new System.Windows.Forms.RadioButton();
             this.rbtnselect = new System.Windows.Forms.RadioButton();
+            this.rbtncreate = new System.Windows.Forms.RadioButton();
             this.btnclear = new System.Windows.Forms.Button();
             this.btnremove = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnteken = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.gblements.SuspendLayout();
             this.gpbmode.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnteken.SuspendLayout();
             this.SuspendLayout();
             // 
             // gblements
@@ -55,16 +57,16 @@
             this.gblements.TabStop = false;
             this.gblements.Text = "Elements";
             // 
-            // rbtnactor
+            // rbtnline
             // 
-            this.rbtnactor.AutoSize = true;
-            this.rbtnactor.Location = new System.Drawing.Point(6, 19);
-            this.rbtnactor.Name = "rbtnactor";
-            this.rbtnactor.Size = new System.Drawing.Size(50, 17);
-            this.rbtnactor.TabIndex = 1;
-            this.rbtnactor.TabStop = true;
-            this.rbtnactor.Text = "Actor";
-            this.rbtnactor.UseVisualStyleBackColor = true;
+            this.rbtnline.AutoSize = true;
+            this.rbtnline.Location = new System.Drawing.Point(6, 64);
+            this.rbtnline.Name = "rbtnline";
+            this.rbtnline.Size = new System.Drawing.Size(45, 17);
+            this.rbtnline.TabIndex = 2;
+            this.rbtnline.TabStop = true;
+            this.rbtnline.Text = "Line";
+            this.rbtnline.UseVisualStyleBackColor = true;
             // 
             // rbtnusecases
             // 
@@ -77,16 +79,16 @@
             this.rbtnusecases.Text = "Use Case";
             this.rbtnusecases.UseVisualStyleBackColor = true;
             // 
-            // rbtnline
+            // rbtnactor
             // 
-            this.rbtnline.AutoSize = true;
-            this.rbtnline.Location = new System.Drawing.Point(6, 64);
-            this.rbtnline.Name = "rbtnline";
-            this.rbtnline.Size = new System.Drawing.Size(45, 17);
-            this.rbtnline.TabIndex = 2;
-            this.rbtnline.TabStop = true;
-            this.rbtnline.Text = "Line";
-            this.rbtnline.UseVisualStyleBackColor = true;
+            this.rbtnactor.AutoSize = true;
+            this.rbtnactor.Location = new System.Drawing.Point(6, 19);
+            this.rbtnactor.Name = "rbtnactor";
+            this.rbtnactor.Size = new System.Drawing.Size(50, 17);
+            this.rbtnactor.TabIndex = 1;
+            this.rbtnactor.TabStop = true;
+            this.rbtnactor.Text = "Actor";
+            this.rbtnactor.UseVisualStyleBackColor = true;
             // 
             // gpbmode
             // 
@@ -99,17 +101,6 @@
             this.gpbmode.TabStop = false;
             this.gpbmode.Text = "Mode";
             // 
-            // rbtncreate
-            // 
-            this.rbtncreate.AutoSize = true;
-            this.rbtncreate.Location = new System.Drawing.Point(10, 18);
-            this.rbtncreate.Name = "rbtncreate";
-            this.rbtncreate.Size = new System.Drawing.Size(56, 17);
-            this.rbtncreate.TabIndex = 0;
-            this.rbtncreate.TabStop = true;
-            this.rbtncreate.Text = "Create";
-            this.rbtncreate.UseVisualStyleBackColor = true;
-            // 
             // rbtnselect
             // 
             this.rbtnselect.AutoSize = true;
@@ -120,6 +111,17 @@
             this.rbtnselect.TabStop = true;
             this.rbtnselect.Text = "Select";
             this.rbtnselect.UseVisualStyleBackColor = true;
+            // 
+            // rbtncreate
+            // 
+            this.rbtncreate.AutoSize = true;
+            this.rbtncreate.Location = new System.Drawing.Point(10, 18);
+            this.rbtncreate.Name = "rbtncreate";
+            this.rbtncreate.Size = new System.Drawing.Size(56, 17);
+            this.rbtncreate.TabIndex = 0;
+            this.rbtncreate.TabStop = true;
+            this.rbtncreate.Text = "Create";
+            this.rbtncreate.UseVisualStyleBackColor = true;
             // 
             // btnclear
             // 
@@ -139,21 +141,40 @@
             this.btnremove.Text = "Remove";
             this.btnremove.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // pnteken
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(19, 22);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(477, 296);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.pnteken.Controls.Add(this.label2);
+            this.pnteken.Controls.Add(this.label1);
+            this.pnteken.Location = new System.Drawing.Point(20, 22);
+            this.pnteken.Name = "pnteken";
+            this.pnteken.Size = new System.Drawing.Size(496, 296);
+            this.pnteken.TabIndex = 4;
+            this.pnteken.Click += new System.EventHandler(this.pnteken_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(676, 331);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnteken);
             this.Controls.Add(this.btnremove);
             this.Controls.Add(this.btnclear);
             this.Controls.Add(this.gpbmode);
@@ -164,7 +185,8 @@
             this.gblements.PerformLayout();
             this.gpbmode.ResumeLayout(false);
             this.gpbmode.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnteken.ResumeLayout(false);
+            this.pnteken.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -180,7 +202,9 @@
         private System.Windows.Forms.RadioButton rbtncreate;
         private System.Windows.Forms.Button btnclear;
         private System.Windows.Forms.Button btnremove;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Panel pnteken;
     }
 }
 
--- Use Case Helper/Form1.cs
+++ Use Case Helper/Form1.cs
@@ -17,5 +17,99 @@ namespace Use_Case_Helper
             InitializeComponent();
         }
 
+        int aantalman = 0;
+        Graphics g;
+
+        private void pnteken_Click(object sender, EventArgs e)
+        {
+            if (rbtnactor.Checked && rbtncreate.Checked)
+            {
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                Point a = Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y);
+
+                if (a.X < 450)
+                {
+                    MessageBox.Show("X = " + a.X.ToString() + " " + "Y = " + a.Y.ToString());
+
+                    if (aantalman == 0)
+                    {
+                        g = pnteken.CreateGraphics();
+
+                        Pen p = new Pen(Color.Black, 3);
+
+                        Point linkervoetb = new Point(20, 80);
+                        Point linkervoete = new Point(60, 60);
+                        Point rechtervoetb = new Point(100, 80);
+                        Point rechtervoete = new Point(60, 60);
+                        Point rugb = new Point(60, 60);
+                        Point ruge = new Point(60, 20);
+                        Point armb = new Point(20, 40);
+                        Point arme = new Point(100, 40);
+
+                        g.DrawLine(p, linkervoetb, linkervoete);
+                        g.DrawLine(p, rechtervoetb, rechtervoete);
+                        g.DrawLine(p, rugb, ruge);
+                        g.DrawLine(p, armb, arme);
+                        g.DrawEllipse(p, 50, 0, 20, 20);
+                    }
+
+                    if (aantalman == 1)
+                    {
+                        g = pnteken.CreateGraphics();
+
+                        Pen p = new Pen(Color.Black, 3);
+
+                        Point linkervoetb = new Point(20, 180);
+                        Point linkervoete = new Point(60, 160);
+                        Point rechtervoetb = new Point(100, 180);
+                        Point rechtervoete = new Point(60, 160);
+                        Point rugb = new Point(60, 160);
+                        Point ruge = new Point(60, 120);
+                        Point armb = new Point(20, 140);
+                        Point arme = new Point(100, 140);
+
+                        g.DrawLine(p, linkervoetb, linkervoete);
+                        g.DrawLine(p, rechtervoetb, rechtervoete);
+                        g.DrawLine(p, rugb, ruge);
+                        g.DrawLine(p, armb, arme);
+                        g.DrawEllipse(p, 50, 100, 20, 20);
+                    }
+
+                    if (aantalman == 2)
+                    {
+                        g = pnteken.CreateGraphics();
+
+                        Pen p = new Pen(Color.Black, 3);
+
+                        Point linkervoetb = new Point(20, 280);
+                        Point linkervoete = new Point(60, 260);
+                        Point rechtervoetb = new Point(100, 280);
+                        Point rechtervoete = new Point(60, 260);
+                        Point rugb = new Point(60, 260);
+                        Point ruge = new Point(60, 220);
+                        Point armb = new Point(20, 240);
+                        Point arme = new Point(100, 240);
+
+                        g.DrawLine(p, linkervoetb, linkervoete);
+                        g.DrawLine(p, rechtervoetb, rechtervoete);
+                        g.DrawLine(p, rugb, ruge);
+                        g.DrawLine(p, armb, arme);
+                        g.DrawEllipse(p, 50, 200, 20, 20);
+                    }
+
+                    if (aantalman > 2)
+                    {
+                        MessageBox.Show("You have reached the maximum number of actors");
+                    }
+
+                    aantalman++;
+                }
+
+                else
+                {
+                    MessageBox.Show("To Create a Actor, Please click on the left side of the program");
+                }
+            }
+        }
     }
 }
